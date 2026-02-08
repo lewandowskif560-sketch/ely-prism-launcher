628a902e9781ba7bd0ad27a20c0eecec4eed9a79
Maks786 <lewandowskif560@gmail.com>
2026-02-08 20:38:44 +0100

    Add ModManager class for mod and modpack management

---
+++ src/main/java/com/elyprism/launcher/mod/ModManager.java
@@ -0,0 +1,20 @@
+package com.elyprism.launcher.mod;
+
+public class ModManager {
+
+    public void installMod(String modName) {
+        // Logic to install a mod
+    }
+
+    public void uninstallMod(String modName) {
+        // Logic to uninstall a mod
+    }
+
+    public void listMods() {
+        // Logic to list installed mods
+    }
+
+    public void manageModPack(String packName) {
+        // Logic to manage a modpack
+    }
+}
